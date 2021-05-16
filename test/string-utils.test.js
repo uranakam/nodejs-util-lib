bed168afd8c87dd49de3c9d42037460f96fa7248
Michael Uranaka <uranakam@gmail.com>
2021-05-15 15:33:37 -1000

    Finishing tests

--- test/string-utils.test.js
+++ test/string-utils.test.js
@@ -2,29 +2,80 @@
 
 const StringUtils = require('../lib/string-utils');
 
-test('isEmpty', () => {
+//======================================================================================
+// isEmpty
+//======================================================================================
+
+test('isEmpty empty value returns true', () => {
   expect(StringUtils.isEmpty()).toBeTruthy();
+});
+
+test('isEmpty null value returns true', () => {
   expect(StringUtils.isEmpty(null)).toBeTruthy();
-  expect(StringUtils.isEmpty(undefined)).toBeTruthy();
+});
+
+test('isEmpty empty string returns true', () => {
   expect(StringUtils.isEmpty('')).toBeTruthy();
+});
+
+test('isEmpty test returns false', () => {
   expect(StringUtils.isEmpty('test')).toBeFalsy();
+});
+
+test('isEmpty empty object returns true', () => {
+  expect(StringUtils.isEmpty({})).toBeTruthy();
+});
+
+test('isEmpty 0 returns false', () => {
   expect(StringUtils.isEmpty(0)).toBeFalsy();
+});
+
+test('isEmpty 1 returns false', () => {
   expect(StringUtils.isEmpty(1)).toBeFalsy();
 });
 
-test('contains', () => {
+//======================================================================================
+// contains
+//======================================================================================
+
+test('contains empty value returns false', () => {
   expect(StringUtils.contains()).toBeFalsy();
-  expect(StringUtils.contains(null, null)).toBeFalsy();
-  expect(StringUtils.contains(undefined, undefined)).toBeFalsy();
+});
+
+test('contains null value returns false', () => {
+  expect(StringUtils.contains(null)).toBeFalsy();
+});
+
+test('contains (test, e) returns true', () => {
   expect(StringUtils.contains('test', 'e')).toBeTruthy();
+});
+
+test('contains (test, E, true) returns false', () => {
   expect(StringUtils.contains('test', 'E', true)).toBeFalsy();
 });
 
-test('prettyPrintJSON', () => {
+//======================================================================================
+// prettyPrintJSON
+//======================================================================================
+
+test('prettyPrintJSON empty value returns empty string', () => {
   expect(StringUtils.prettyPrintJSON()).toBe('');
+});
+
+test('prettyPrintJSON null value returns empty string', () => {
   expect(StringUtils.prettyPrintJSON(null)).toBe('');
-  expect(StringUtils.prettyPrintJSON(undefined)).toBe('');
+});
+
+test('prettyPrintJSON empty object returns {}', () => {
   expect(StringUtils.prettyPrintJSON({})).toBe('{}');
-  expect(StringUtils.prettyPrintJSON({ "test": "test" })).toBe('{\n  \"test\": \"test\"\n}');
-  expect(StringUtils.prettyPrintJSON({ "test": "test" }, 4)).toBe('{\n    \"test\": \"test\"\n}');
+});
+
+test('prettyPrintJSON { "test": "test" } returns correct format', () => {
+  expect(StringUtils.prettyPrintJSON({ "test": "test" }))
+    .toBe('{\n  \"test\": \"test\"\n}');
+});
+
+test('prettyPrintJSON ({ "test": "test" }, 4) returns correct format', () => {
+  expect(StringUtils.prettyPrintJSON({ "test": "test" }, 4))
+    .toBe('{\n    \"test\": \"test\"\n}');
 });
--- test/timer-utils.test.js
+++ test/timer-utils.test.js
@@ -2,10 +2,50 @@
 
 const TimerUtils = require('../lib/timer-utils');
 
-test('getTimeDifferenceString', () => {
+//======================================================================================
+// getTimeDifferenceString
+//======================================================================================
 
+test('getTimeDifferenceString null returns empty string', () => {
+  expect(TimerUtils.getTimeDifferenceString(null, null))
+    .toBe('');
 });
 
-test('formatTime', () => {
+test('getTimeDifferenceString null returns empty string', () => {
+  expect(TimerUtils.getTimeDifferenceString(12345, 132450))
+    .toBe('2 Minute(s) 0 Second(s)');
+});
 
+//======================================================================================
+// formatTime
+//======================================================================================
+
+test('formatTime null returns empty string', () => {
+  expect(TimerUtils.formatTime(null))
+    .toBe('');
+});
+
+test('formatTime empty value returns empty string', () => {
+  expect(TimerUtils.formatTime())
+    .toBe('');
+});
+
+test('formatTime 1 returns 1 Millisecond(s)', () => {
+  expect(TimerUtils.formatTime(1))
+    .toBe('1 Millisecond(s)');
+});
+
+test('formatTime 1000 returns 1 Second(s)', () => {
+  expect(TimerUtils.formatTime(1000))
+    .toBe('1 Second(s)');
+});
+
+test('formatTime 60000 returns 1 Minute(s) 0 Second(s)', () => {
+  expect(TimerUtils.formatTime(60000))
+    .toBe('1 Minute(s) 0 Second(s)');
+});
+
+test('formatTime 6000000 returns 1 Hour(s) 0 Minute(s) 0 Second(s)', () => {
+  expect(TimerUtils.formatTime(3600000))
+    .toBe('1 Hour(s) 0 Minute(s) 0 Second(s)');
 });
